f7cc89f5cf329ad6b6e3fd30f96c9ae39e9a907c
Nick Partridge <nkpart@gmail.com>
2013-09-03 20:27:19 +1000

    .

--- src/PresenterMain.hs
+++ src/PresenterMain.hs
@@ -17,15 +17,21 @@ import qualified Data.Set as Set (insert, delete)
 import Data.Set (Set)
 import Graphics.UI.SDL as SDL
 import Graphics.UI.SDL.Image as SDLI
+import Graphics.UI.SDL.TTF as SDLF
 
--- data Loop = TapToStart
-          -- | Play
-          -- | ShowVictory GameState Sky
+data Slider = Slider {
+              _sliderSurface :: Surface
+            , _sliderTitleFont :: Font
+            , _sliderSheep :: Surface
+  }
 
--- data SDLRenderer = SDLRenderer {
-                    -- _screen :: SDL.Surface,
-                    -- _sheep :: SDL.Surface
-                  -- }
+withSlider f = SDL.withInit [SDL.InitEverything] $ do
+  SDLF.init
+  window <- SDL.setVideoMode 800 600 32 [SDL.SWSurface]
+  font <- SDLF.openFont "SourceCodePro-Medium.ttf" 28
+  sheep <- SDLI.load "sheep.png"
+  f $ Slider window font sheep
+  SDLF.quit
 
 data Slide = Title String
            | forall a b e. (Show e, Show a, Show b) => DisplayWire (Wire e IO a b)
@@ -44,17 +50,28 @@ navigate GoRight (ls, f, r:rs) = (f:ls, r, rs)
 slidePresenterW :: (Slide, [Slide]) -> Wire e m (Maybe Nav) Slide
 slidePresenterW = undefined
 
+renderSlide slide surface titleFont = do
+  return ()
+
 main :: IO ()
-main = SDL.withInit [SDL.InitEverything] $ do
-    window <- SDL.setVideoMode 800 600 32 [SDL.SWSurface]
-    sheep <- SDLI.load "sheep.png"
-    loop window sheep
+main = withSlider $ \slider -> do
+    go slider $ [Title "Hi There 2", Title "Next"]
     return ()
   where
-    loop window sheep = do
-      SDL.blitSurface sheep Nothing window $ Just $ SDL.Rect 0 0 100 100
-      SDL.flip window
-      loop window sheep
+    go slider slides = go_ clockSession $ pure $ head slides
+      where go_ s w = do
+                (r, w, s) <- stepSession_ w s ()
+                case r of 
+                  Title xxx -> do
+                    SDL.blitSurface sheep Nothing window $ Just $ SDL.Rect 0 0 100 100
+                    text <- SDLF.renderTextShaded font xxx (SDL.Color 255 255 255) (SDL.Color 0 0 0)
+                    SDL.blitSurface text Nothing window $ Just $ SDL.Rect 100 100 100 100
+                    SDL.flip window
+                  DisplayWire _ -> return ()
+                go_ s w
+            window = _sliderSurface slider
+            sheep = _sliderSheep slider
+            font = _sliderTitleFont slider
 
 keysDownW = mkStateM mempty $ \_ (_, keys) -> do
   newKeys <- parseEvents keys
